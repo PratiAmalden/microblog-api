42ba43efb713e0a7bcec9b6df48cbb9206ceb067
pratiamalden <pratiamalden@gmail.com>
2026-02-23 20:47:54 +0000

    feat(auth): customize Devise routes and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users,
+    path: "",
+    path_names: {
+      sign_in: "users/signin",
+      sign_out: "users/signout"
+    },
+    controllers: {
+      sessions: "users/sessions",
+      registrations: "users/registrations"
+    }
+
+  devise_scope :user do
+    post "users/signup", to: "users/registrations#create"
+  end
 
   get "up" => "rails/health#show", as: :rails_health_check
 
