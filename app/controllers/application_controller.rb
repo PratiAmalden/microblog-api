b13d807bddcbc787c4756aab862cd42eaeedc53b
pratiamalden <pratiamalden@gmail.com>
2026-02-28 21:11:53 +0000

    feat(controller): add Pagy for pagination and handle ActiveRecord::RecordNotFound

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::API
+  include Pagy::Backend
+
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
@@ -6,5 +8,9 @@ class ApplicationController < ActionController::API
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [ :name ])
     devise_parameter_sanitizer.permit(:account_update, keys: [ :name ])
+  end
+
+  rescue_from ActiveRecord::RecordNotFound do
+    render json: { error: "Not found" }, status: :not_found
   end
 end
