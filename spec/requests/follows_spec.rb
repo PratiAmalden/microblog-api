73b93c828235cf5f49465fac2d3268bd0b8c0626
pratiamalden <pratiamalden@gmail.com>
2026-03-09 21:58:50 +0000

    test(follows): update follow request specs for JWT auth and unfollow params

--- spec/requests/follows_spec.rb
+++ spec/requests/follows_spec.rb
@@ -1,72 +1,67 @@
 require 'rails_helper'
 
 RSpec.describe "Follows", type: :request do
-  let(:user_a) { create(:user) }
-  let(:user_b) { create(:user) }
+  let(:user) { create(:user) }
+  let(:other_user) { create(:user) }
+  let(:headers) { auth_headers(user) }
+  let(:followed_id) { other_user.id }
 
-  describe "POST /follows" do
-    subject(:do_request) { post follow_path, params: params }
-
+  describe "POST /follow" do
     let(:params) { { follow: { followed_id: followed_id } } }
-    let(:followed_id) { user_b.id }
+
+    subject(:do_request) { post follow_path, params: params, headers: headers }
 
     context "when authenticated" do
-      before { sign_in user_a }
-
-      it "creates the follow" do
+      it "creates a follow relationship" do
         expect { do_request }.to change(Follow, :count).by(1)
         expect(response).to have_http_status(:created)
-        expect(Follow.exists?(follower: user_a, followed: user_b)).to be(true)
+        expect(Follow.exists?(follower: user, followed: other_user)).to be true
       end
     end
 
     context "when trying to follow self" do
-      before { sign_in user_a }
-      let(:followed_id) { user_a.id }
+      let(:followed_id) { user.id }
 
-      it "returns an error" do
+      it "returns unprocessable entity" do
         expect { do_request }.not_to change(Follow, :count)
         expect(response).to have_http_status(:unprocessable_entity)
       end
     end
 
-    context "when unauthorized follow" do
-      it "return unauthorized" do
-        expect { do_request }.not_to change(Follow, :count)
+    context "when unauthorized" do
+      it "returns unauthorized" do
+        expect { post follow_path, params: params }.not_to change(Follow, :count)
         expect(response).to have_http_status(:unauthorized)
       end
     end
 
     context "when already following" do
-      before do
-        sign_in user_a
-        create(:follow, follower: user_a, followed: user_b)
-      end
+      before { create(:follow, follower: user, followed: other_user) }
+
       it "doesn't create duplicate follow" do
         expect { do_request }.not_to change(Follow, :count)
         expect(response).to have_http_status(:unprocessable_entity)
       end
     end
-
   end
 
-  describe "POST /follows" do
-    it "unfollows a user" do
-      sign_in user_a 
-      create(:follow, follower: user_a, followed: user_b)
+  describe "POST /unfollow" do
+    let!(:follow) { create(:follow, follower: user, followed: other_user) }
+    let(:params) { { followed_id: followed_id } }
 
+    subject(:do_request) { post unfollow_path, params: params, headers: headers }
+
+    it "unfollows a user" do
       expect {
-        post unfollow_path, params: { followed_id: user_b.id }
+        do_request
       }.to change(Follow, :count).by(-1)
       expect(response).to have_http_status(:no_content)
-      expect(Follow.exists?(follower: user_a, followed: user_b)).to be(false)
+      expect(Follow.exists?(follower: user, followed: other_user)).to be(false)
     end
 
     it "return unauthorized" do
-      create(:follow, follower: user_a, followed: user_b)
-
       expect {
-        post unfollow_path, params: { followed_id: user_b.id }
+        post unfollow_path, params: params
       }.not_to change(Follow, :count)
       expect(response).to have_http_status(:unauthorized)
     end
