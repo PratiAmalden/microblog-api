bc4803bfc203cf10a0cb065493551431c8ba2a39
pratiamalden <pratiamalden@gmail.com>
2026-03-09 22:11:42 +0000

    test(user): improve User model specs for feed and follow behavior

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,47 +1,59 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
+    let(:user) { create(:user) }
+    let(:other_user) { create(:user) }
+
   describe "validations" do
-    it "is valid with a new name and email" do
-      user = build(:user)
-      expect(user).to be_valid
+    it "is invalid without an email" do
+      expect(build(:user, email: nil)).not_to be_valid
     end
   end
 
   describe "#feed" do
-    let(:user_a) { create(:user) }
-    let(:user_b) { create(:user) }
-
     it "includes user's own posts" do
-      micropost = create(:micropost, user: user_a)
-      expect(user_a.feed).to include(micropost)
+      micropost = create(:micropost, user: user)
+      expect(user.feed).to include(micropost)
     end
 
     it "includes followed users posts" do
-      create(:follow, follower: user_a, followed: user_b)
-      micropost = create(:micropost, user: user_b)
+      create(:follow, follower: user, followed: other_user)
+      micropost = create(:micropost, user: other_user)
 
-      expect(user_a.feed).to include(micropost)
+      expect(user.feed).to include(micropost)
     end
 
     it "retrieve comment counts per post" do
-      posts_with_comments = create(:micropost, :with_comments, user: user_a)
+      posts_with_comments = create(:micropost, :with_comments, user: user)
 
-      feed_posts = user_a.feed.to_a
-
-      feed_post = feed_posts.find { |p| p.id == posts_with_comments.id }
-
-      expect(feed_post.comments_count).to eq(3)
+      expect(user.feed.find(posts_with_comments.id).comments_count).to eq(3)
     end
   end
 
-  describe "following" do
-    it "can follow another user" do
-      follower = create(:user)
-      followed = create(:user)
-
-      follower.followings << followed
-      expect(follower.followings).to include(followed)
+  describe "#follow" do
+    it "creates a follow relationship" do
+      expect {
+        user.follow(other_user)
+      }.to change(Follow, :count).by(1)
+      expect(user.followings).to include(other_user)
     end
+  end
+
+  describe "#unfollow" do
+    before do
+      user.follow(other_user)
+    end
+
+    it "removes the follow relationship" do
+      expect {
+        user.unfollow(other_user)
+      }.to change(Follow, :count).by(-1)
+    end
+  end
+
+  it "does not allow following self" do
+    expect {
+      user.follow(user)
+    }.not_to change(Follow, :count)
   end
 end
