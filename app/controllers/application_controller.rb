340c8e9fcd21aad8343681521db29f0d99f358ee
pratiamalden <pratiamalden@gmail.com>
2026-04-23 17:18:05 +0100

    feat: add ActionController error handling and move not found rescue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,18 @@ class ApplicationController < ActionController::API
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  rescue_from ActiveRecord::RecordNotFound do
+    render json: { errors: [ { message: "Not found" } ] }, status: :not_found
+  end
+
+  rescue_from ActionController::ParameterMissing do |e|
+    render json: { errors: [ { message: "Required parameter is missing or invalid: #{e.param}" } ] }, status: :bad_request
+  end
+
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [ :name ])
     devise_parameter_sanitizer.permit(:account_update, keys: [ :name ])
-  end
-
-  rescue_from ActiveRecord::RecordNotFound do
-    render json: { error: "Not found" }, status: :not_found
   end
 end
